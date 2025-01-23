b258f236b0bd1df1e21e9721c25be32e823f294c
timkenar <chepkenertimothy@gmail.com>
2025-01-23 14:32:58 +0300

    Created a working landing page

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import LandingPage from "./pages/LandingPage";
 
 const App = () => {
   return (
-    <Router>
+   
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
       </Routes>
-    </Router>
+   
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App"; // Assuming your main app component is in App.tsx
-import "./index.css"; // Import your global styles if needed
-
+import App from "./App"; 
+import "./index.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
