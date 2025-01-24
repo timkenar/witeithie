2940e36d50438c9f731a9e2dba88f1fda3d16ebd
Timothy Chepkener <97870990+timkenar@users.noreply.github.com>
2025-01-24 10:46:47 +0300

    Update axios.js to add the online backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000", // Replace with your backend URL
+  baseURL: "http://127.0.0.1:8000","https://witeithie-backend.onrender.com", // Replace with your backend URL
   headers: {
     "Content-Type": "application/json",
   },
